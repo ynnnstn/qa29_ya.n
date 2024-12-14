da91234b4de4eec447f7b9f16b7abfd62f4fde94
ynnnstn <yatsuk03@mail.ru>
2024-12-14 12:54:45 +0300

    lesson 5

---
+++ src/main/java/lesson51/Student.java
@@ -0,0 +1,23 @@
+package lesson51;
+
+    public class Student {
+        public String firstname;
+        public String lastname;
+        public int age;
+        public int lecture;
+
+        public Student(String firstname, String lastname, int age, int lecture) {
+            this.firstname= firstname;
+            this.lastname= lastname;
+            this.age= age;
+            this.lecture=lecture;
+        }
+
+        public String toString() {
+            return "Student{" +
+                    "firstName='" + firstname + '\'' + ", lastName='" + lastname + '\'' + ", age=" + age + ", lectureCount=" + lecture + '}';
+
+        }
+    }
+
+
