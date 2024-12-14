c8cafb5bd5b3a784b9bcef17a64d23c6187763cb
ynnnstn <yatsuk03@mail.ru>
2024-12-14 12:22:08 +0300

    lessom 5 done

---
+++ src/main/java/lesson5/Main.java
@@ -0,0 +1,10 @@
+package lesson5;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student student = new Student("Nastya", "Yatsuk", 21, 7);
+        System.out.println(student);
+    }
+}
+
---
+++ src/main/java/lesson5/Student.java
@@ -0,0 +1,22 @@
+package lesson5;
+
+public class Student {
+    public String firstname;
+    public String lastname;
+    public int age;
+    public int lecture;
+
+    public Student(String firstname, String lastname, int age, int lecture) {
+        this.firstname= firstname;
+        this.lastname= lastname;
+        this.age= age;
+        this.lecture=lecture;
+    }
+
+
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstname + '\'' + ", lastName='" + lastname + '\'' + ", age=" + age + ", lectureCount=" + lecture + '}';
+
+    }
+}
